3a7ca2ea36a48956ce5443c5fa21b8b59efac2e3
urostripunovic <39649964+urostripunovic@users.noreply.github.com>
2023-09-07 14:18:30 +0200

    Done

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,34 @@
-import { PropsWithChildren } from 'react'
+import {
+    ButtonHTMLAttributes,
+    CSSProperties,
+    PropsWithChildren,
+    forwardRef,
+} from "react";
 
-export interface ButtonProps {
-  variant?: 'primary' | 'accent' | 'success' | 'danger'
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: "primary" | "accent" | "success" | "danger";
 }
 
-export function Button({ children }: PropsWithChildren<ButtonProps>) {
-  /**
-   * TODO: Implement the Button component.
-   */
+const colorVariants = {
+    primary: "--primary-color",
+    accent: "--accent-color",
+    success: "--success-color",
+    danger: "--danger-color",
+};
 
-  return <>{children}</>
-}
+export const Button = forwardRef<
+    HTMLButtonElement,
+    PropsWithChildren<ButtonProps>
+>(({ children, variant = "primary", ...props }, ref) => {
+    const buttonStyle: CSSProperties = {
+        backgroundColor: `var(${colorVariants[variant]})`,
+        cursor: "pointer",
+        color: "white",
+    };
+
+    return (
+        <button ref={ref} {...props} style={buttonStyle}>
+            {children}
+        </button>
+    );
+});
--- src/components/OrdersList/OrdersList.tsx
+++ src/components/OrdersList/OrdersList.tsx
@@ -1,19 +1,105 @@
-// use this function to fetch the orders.
+import { useEffect, useRef, useState } from "react";
 import { getOrders } from "../../api/api";
+import { Button } from "../Button/Button";
+import "./OrdersList.css";
 
-export function OrdersList(){
+export function OrdersList() {
+    const ref = useRef(null);
+    const [orders, setOrders] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [view, setView] = useState({
+        click: false,
+        order: undefined,
+        variant: "primary",
+    });
 
-  /**
-   * TODO: Implement the OrdersList component.
-   *
-   * Fetch the orders from the API and display them in a list/table.
-   *
-   * use the imported function to fetch the orders from the API: getOrders()
-   */
+    useEffect(() => {
+        const fetchOrders = async () => {
+            try {
+                const orders = await getOrders();
+                setOrders(orders);
+                setLoading(false);
+            } catch (error) {
+                setLoading(true);
+                throw "Fel med att hämta ordrar: " + error;
+            }
+        };
+        fetchOrders();
+    }, []);
 
-  return (
-    <>
-      <h1>Orders List</h1>
-    </>
-  );
+    //console.log(orders);
+
+    return (
+        <>
+            <h1>Orders List</h1>
+            {isLoading ? (
+                <p>Hämtar ordrar...</p>
+            ) : (
+                <>
+                    <table id="orders">
+                        <thead>
+                            <tr>
+                                <th>Ordernummer</th>
+                                <th>Datum</th>
+                                <th>Fullständigt namn</th>
+                                <th>Antal produkter</th>
+                                <th>Summa</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {orders.map((order: any) => {
+                                const totalSum = order.products.reduce(
+                                    (sum: number, product: any) =>
+                                        sum + product.price * product.quantity,
+                                    0
+                                );
+                                const fullName =
+                                    order.first_name + " " + order.last_name;
+                                return (
+                                    <tr
+                                        key={order.id}
+                                        onClick={() =>
+                                            setView({
+                                                click: true,
+                                                order: order.id,
+                                                variant: "success",
+                                            })
+                                        }
+                                    >
+                                        <td>{order.id}</td>
+                                        <td>
+                                            {order.created_at.split(":")[0]}
+                                        </td>
+                                        <td>{fullName}</td>
+                                        <td>{order.products.length}</td>
+                                        <td>{totalSum} kr</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+
+                    <Button
+                        ref={ref}
+                        variant={view.variant}
+                        onClick={() => {
+                            alert(
+                                `Order: ${view.order}` ||
+                                    "Välja gärna en order du vill kolla på"
+                            );
+                            setView({
+                                click: false,
+                                order: undefined,
+                                variant: "primary",
+                            });
+                        }}
+                    >
+                        {!view.click
+                            ? "Kolla på en order i mer detalj"
+                            : `En detaljerad vy av order ${view.order} är redo`}
+                    </Button>
+                </>
+            )}
+        </>
+    );
 }
